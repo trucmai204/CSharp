0e540ed6bd11d791090155a00cba6815ad4a6cab
Truc Mai cua Quang <mtruc7097@gmail.com>
2024-07-07 22:09:24 +0700

    1day

---
+++ TypeInCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TypeInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    int a = 3;
+            //    System.Console.WriteLine(a.ToString());//System là không gian tên chứa các lớp cơ bản của .NET Framework. 
+            // bool là đại diện (tương đương) với System.Boolean(kiểu này ít sử dụng)
+            string s, b, q;// khai báo chuỗi trong ""
+            s = Console.ReadLine();
+            b = @"uegfwiego\n wegrwi\r";// thêm ký tự @ thì mọi hoạt động bên trong chuỗi sẽ không thực hiện
+            q = $"fgyuweyfi {DateTime.Now}";// $ có thể nhúng các biến, biểu thức 
+            Console.WriteLine(q);
+        }
+    }
+}
