a4f2afe1dc68c5b0f4fe0a231c14a17547968d0d
Truc Mai cua Quang <mtruc7097@gmail.com>
2024-07-11 08:30:14 +0700

    newx2

---
+++ MaxMin/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Linq;
+
+namespace MaxMin
+{
+    internal class Program
+    {
+        public class Integer
+        {
+            public List<int> inter {  get; set; }
+
+            public void Input()
+            {
+                Console.WriteLine("Nhập các số nguyên: ");
+                string input = Console.ReadLine();
+                inter = input.Split(' ').Select(int.Parse).ToList();
+            }
+            public int GetMaxNumber()
+            {
+                int max = int.MinValue;
+      
+                foreach (var num in inter)
+                {
+                    if (num > max)
+                    {
+                        max = num;
+                    }
+                }
+                return max;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Integer number = new Integer();
+            number.Input();
+            
+            Console.WriteLine("Số lơn nhất trong dãy số là:" +number.GetMaxNumber());
+        }
+    }
+}
--- TypeInCSharp/Program.cs
+++ TypeInCSharp/Program.cs
@@ -1,17 +1,39 @@
-﻿namespace TypeInCSharp
+﻿using System.Text;
+
+namespace TypeInCSharp
 {
     internal class Program
     {
+        int? x; // x có giá trị và có thể bị null
         static void Main(string[] args)
         {
             //    int a = 3;
             //    System.Console.WriteLine(a.ToString());//System là không gian tên chứa các lớp cơ bản của .NET Framework. 
             // bool là đại diện (tương đương) với System.Boolean(kiểu này ít sử dụng)
+
+            /** String
             string s, b, q;// khai báo chuỗi trong ""
             s = Console.ReadLine();
-            b = @"uegfwiego\n wegrwi\r";// thêm ký tự @ thì mọi hoạt động bên trong chuỗi sẽ không thực hiện
+            b = @"uegfwiego\n wegrwi\r";// interpolation string, thêm ký tự @ thì mọi hoạt động bên trong chuỗi sẽ không thực hiện
             q = $"fgyuweyfi {DateTime.Now}";// $ có thể nhúng các biến, biểu thức 
-            Console.WriteLine(q);
+            
+            // sử dụng stringbuilder tối ưu hóa hơn phép cộng chuỗi
+            StringBuilder sb = new StringBuilder();//
+            sb = sb.Append(s);
+            sb = sb.Append(b);
+            sb = sb.Append(q);// stringbuilder sẽ chuyển string thành các công lệnh cụ thể để tối ưu hiệu năng
+
+            string m = sb.ToString();
+
+            Console.WriteLine(m);*/
+
+            //Dynamic: bỏ qua bước kiểm tra trình dịch
+
+            var x = 10;//dựa vào dữ liệu nhập thì nó sẽ tự xác định kiểu dữ liệu tương ứng
+            
+            int? y = 0;
+            y.HasValue.ToString();// hasvalue sẽ kireemr tra xem biến có tồn tại giá trị nào hay không
+            Console.WriteLine("abcdfghjk".ToLower());// chuỗi có thể coi là 1 hằng (object)
         }
     }
 }
